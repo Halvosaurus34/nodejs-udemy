bfc2b8fafbce02257920e7815961fb819e22f52e
Levi <lhalvorson34@gmail.com>
2021-04-08 17:58:46 -0600

    finished assignment1

--- node1/app.js
+++ node1/app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+  }
+  res.setHeader("Header-Type", "text/html");
+  res.write();
 });
 
 server.listen(3000);
